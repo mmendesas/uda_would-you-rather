f3b3946600b48d8361ae4475119de0d3e0a039d2
Marcio Mendes <mmendes.as@gmail.com>
2019-04-16 10:27:30 -0300

    fix:  user score as stateless and update table warning

--- src/components/UserScore.js
+++ src/components/UserScore.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class UserScore extends Component {
-  render() {
-    const { user } = this.props;
-    const { id, name, avatarURL, answers, questions } = user;
+const UserScore = ({ user }) => {
+  const { id, name, avatarURL, answers, questions } = user;
 
-    const numAnswers = Object.keys(answers).length;
-    const numQuestions = questions.length;
+  const numAnswers = Object.keys(answers).length;
+  const numQuestions = questions.length;
 
-    if (!user) {
-      return <p>This user doesn't exist</p>;
-    }
+  if (!user) {
+    return <p>This user doesn't exist</p>;
+  }
 
-    return (
-      <div className="user-info">
-        <img src={avatarURL} alt={`Avatar of ${name}`} className="avatar" />
-        <div className="divider" />
-        <div>
-          <h3 className="center">{name}</h3>
-          <table>
+  return (
+    <div className="user-info">
+      <img src={avatarURL} alt={`Avatar of ${name}`} className="avatar" />
+      <div className="divider" />
+      <div>
+        <h3 className="center">{name}</h3>
+        <table>
+          <tbody>
             <tr>
               <td>Answered Questions</td>
               <td>{numAnswers}</td>
@@ -28,16 +27,16 @@ class UserScore extends Component {
               <td>Created Questions</td>
               <td>{numQuestions}</td>
             </tr>
-          </table>
-        </div>
-        <div className="divider" />
-        <div className="center">
-          <div className="scoreTotal">{numAnswers + numQuestions}</div>
-          <div>score</div>
-        </div>
+          </tbody>
+        </table>
       </div>
-    );
-  }
+      <div className="divider" />
+      <div className="center">
+        <div className="scoreTotal">{numAnswers + numQuestions}</div>
+        <div>score</div>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = ({ users }, { id }) => ({
